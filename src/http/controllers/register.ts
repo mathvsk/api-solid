d21b305d613b94595863659ed68cc680638a779d
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-25 20:37:28 -0300

    refactor: criar use case para criação de usuario

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 import { hash } from 'bcryptjs'
+import { registerUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,27 +13,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send({
-      message: 'Email ja cadastrado',
-    })
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error: any) {
+    return reply.status(400).send({ message: error.message })
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
 
   return reply.status(201).send()
 }
