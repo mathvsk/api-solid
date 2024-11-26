c856ab8874054d5bb7b87c5747c6ecf084f4e9d8
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-25 21:30:59 -0300

    feat: criar erro personalizado

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -21,8 +22,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       email,
       password
     })
-  } catch (error: any) {
-    return reply.status(400).send({ message: error.message })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+    
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
