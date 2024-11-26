c808d878457a769cb1d61b2f82a07dfcb218878d
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-26 07:45:55 -0300

    feat: criar pattern de "in-memory-repository"

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,30 @@
+import { User, Prisma } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+
+export class InMemoryUsersRepository implements UsersRepository{
+    public users: User[] = []
+
+    async findByEmail(email: string): Promise<User | null> {
+        const emailAlreadyExists = this.users.find(user => user.email === email)
+
+        if(!emailAlreadyExists) {
+            return null
+        }
+
+        return emailAlreadyExists
+    }
+    
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        const user = {
+            id: 'user-1',
+            name: data.name,
+            email: data.email,
+            password_hash: data.password_hash,
+            created_at: new Date(),
+        }
+
+        this.users.push(user)
+
+        return user
+    }
+}
