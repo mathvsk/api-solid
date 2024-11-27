583bf1a50b91985dc7c2f5cbfa6995c965acf154
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-26 22:56:52 -0300

    feat: criar teste unitario para check in

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,21 @@
+import { Checkin, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { CheckInsRepository } from '../check-ins-repository'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public checkIns: Checkin[] = []
+
+  async create(data: Prisma.CheckinUncheckedCreateInput): Promise<Checkin> {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
+    }
+
+    this.checkIns.push(checkIn)
+
+    return checkIn
+  }
+}
