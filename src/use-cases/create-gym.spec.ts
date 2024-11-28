173f000bb6e8949600ea1d295d55813b440d4d85
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-27 21:18:59 -0300

    feat: criar unit test para criação de academia

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: null,
+      phone: null,
+      latitude: 25.5512482,
+      longitude: -49.2611328,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
