a85dcea03deef9e63fcbab70b7bfef88cffaff12
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-28 20:55:55 -0300

    feat: criar use case de buscar academia

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,31 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    if (!gyms?.length) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      gyms,
+    }
+  }
+}
