3d3fc61d1f898dab79fb78df7c8b78fdf0bca709
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-28 20:13:55 -0300

    feat: criar metodo para recuperar o total de checkin realizado

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -35,6 +35,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .slice((page - 1) * 20, page * 20)
   }
 
+  async countByUserId(userId: string): Promise<number> {
+    return this.checkIns.filter((checkIn) => checkIn.user_id === userId).length
+  }
+
   async create(data: Prisma.CheckinUncheckedCreateInput): Promise<Checkin> {
     const checkIn = {
       id: randomUUID(),
