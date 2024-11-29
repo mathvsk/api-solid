6c475fbbecb5dd6e29ff413ce00d98babda19792
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-29 20:45:40 -0300

    feat: criar e apagar schema criado em cada switch de teste e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,4 +1,22 @@
-import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+
+import { randomUUID } from 'node:crypto'
+import { Environment } from 'vitest/environments'
+
+const prisma = new PrismaClient()
+
+function generateNewDatabaseURL(newSchema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Por favor, forneça uma variável de ambiente DATABASE_URL.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', newSchema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   transformMode: 'ssr',
@@ -7,9 +25,21 @@ export default <Environment>{
   async setup() {
     console.log('Setup')
 
+    const schema = randomUUID()
+    const databaseURL = generateNewDatabaseURL(schema)
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
       async teardown() {
         console.log('Teardown')
+
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
