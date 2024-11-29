8842a1fab253fb0b8b063bf735c372d485726eb8
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-29 08:02:06 -0300

    feat: criar prisma repository de gyms

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,43 @@
+import { prisma } from '@/lib/prisma'
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string): Promise<Gym | null> {
+    const gym = prisma.gym.findUnique({ where: { id } })
+
+    return gym
+  }
+
+  async findManyNearby({
+    latitude,
+    longitude,
+  }: FindManyNearbyParams): Promise<Gym[]> {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+        SELECT * from gyms
+        WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = prisma.gym.create({ data })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number): Promise<Gym[] | null> {
+    const gyms = prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+}
