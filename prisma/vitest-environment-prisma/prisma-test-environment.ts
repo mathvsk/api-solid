b8040956c525c1f4484ce31208429ecf991af38d
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-29 20:05:07 -0300

    feat: criar ambiente separado para testes e2e

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,16 @@
+import { Environment } from 'vitest'
+
+export default <Environment>{
+  transformMode: 'ssr',
+  name: 'prisma',
+
+  async setup() {
+    console.log('Setup')
+
+    return {
+      async teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+}
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    plugins: [tsconfigPaths()],
-    test: {
-		globals: true,
-		coverage: {
-			all: false,
-		},
-	},
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    coverage: {
+      all: false,
+    },
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+  },
 })
