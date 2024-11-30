45ace161e69abe948c3a373257f9d51e0f049d6f
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-29 21:18:59 -0300

    feat: criar e configurar rotas restantes de "gyms"

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,32 @@
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { q, page } = searchGymsQuerySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  try {
+    const { gyms } = await searchGymsUseCase.execute({
+      query: q,
+      page,
+    })
+
+    return reply.status(200).send({
+      gyms,
+    })
+  } catch (error) {
+    if (error instanceof ResourceNotFoundError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    throw error
+  }
+}
