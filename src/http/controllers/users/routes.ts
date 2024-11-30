edd3d2aa899c7c9ef591674b7d2849e60e8c153a
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-11-29 21:20:09 -0300

    refactor: remover importacao do app

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,10 +1,10 @@
-import { app } from '@/app'
+import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
 
-export function usersRoutes() {
+export function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
