9551fee033969e995c9a5591b08522bcc10580f0
Matheus Viscki <matheusviscki@gmail.com>
2024-12-03 22:13:34 -0300

    refactor: alterar rota de criação

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -11,7 +11,7 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
 
-  app.post('/gyms/:gymId/check-ins', create)
+  app.post('/check-ins/gym/:gymId', create)
 
   app.patch(
     '/check-ins/:checkInId/validate',
